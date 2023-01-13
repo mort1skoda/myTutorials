e4b76a0f7fcba8c8a1834d1ec87f9aa5f4140748
mort1skoda <mort1skoda@gmail.com>
2023-01-13 10:04:10 +0100

    modified:   gp.sh

--- gp.sh
+++ gp.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
+pwd
+REPO_NAME=$(pwd)
+echo "REPO_NAME=" $REPO_NAME
+
+
 git init
-git remote add origin https://github.com/mort1skoda/test.git
+git remote add origin https://github.com/mort1skoda/$REPO_NAME.git
+#exit
 git remote -v
 git add --all
 git status
 git commit
 git push
+
 
 

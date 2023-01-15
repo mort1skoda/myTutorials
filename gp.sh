b729dda149bfe49664276b4980074a0b8351a444
mort1skoda <mort1skoda@gmail.com>
2023-01-15 23:40:07 +0100

    new file:   .tutGit.md 	new file:   .tutHome.md 	new file:   .tutMd.md  	new file:   .tutTmux.md  	new file:   .tutVim.md  	modified:   gp.sh

--- gp.sh
+++ gp.sh
@@ -4,15 +4,13 @@ pwd
 REPO_NAME=$(pwd)
 echo "REPO_NAME=" $REPO_NAME
 
-
 git init
-git remote add origin https://github.com/mort1skoda/$REPO_NAME.git
+#git remote add origin https://github.com/mort1skoda/$REPO_NAME.git
 #exit
 git remote -v
 git add --all
 git status
 git commit
 git push
-
 
 
